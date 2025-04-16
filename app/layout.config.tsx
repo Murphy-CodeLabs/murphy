6add97dbe8e0bedfc6d4c31644d3b30371837a24
lou1s vuong <lou1svuong.dev@gmail.com>
2025-04-16 19:29:25 +0700

    Add Body component and useMode hook for dynamic layout. Updated layout.config.tsx with new app name and GitHub URL.

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,4 +1,4 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
 /**
  * Shared layout configurations
@@ -19,15 +19,9 @@ export const baseOptions: BaseLayoutProps = {
         >
           <circle cx={12} cy={12} r={12} fill="currentColor" />
         </svg>
-        My App
+        Murphis
       </>
     ),
   },
-  links: [
-    {
-      text: 'Documentation',
-      url: '/docs',
-      active: 'nested-url',
-    },
-  ],
+  githubUrl: "https://github.com/murphis-dev/murphis-sdk",
 };
