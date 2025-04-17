903e0437a966de16c219c48909c9202c3b754bda
lou1s vuong <lou1svuong.dev@gmail.com>
2025-04-17 17:17:32 +0700

    Add new Examples page component with initial content layout

---
+++ app/(home)/examples/page.tsx
@@ -0,0 +1,8 @@
+export default function Examples() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1>Example</h1>
+      <p>This is an example page. Coming soon...</p>
+    </div>
+  );
+}
