bc4bd0b6b7ee1fc97c62c9af465b2160c939b8ea
lou1s <lou1svuong.dev@gmail.com>
2025-04-20 03:34:15 +0700

    Add index.tsx file to export documentation components
    - Created a new index.tsx file to consolidate exports for InstallationCommands, DependenciesInstallCommands, and OpenInV0Button components, streamlining the import process for documentation purposes.

---
+++ components/docs/index.tsx
@@ -0,0 +1,5 @@
+import InstallationCommands from "./installation-commands";
+import DependenciesInstallCommands from "./dependencies-install-commands";
+import OpenInV0Button from "./open-in-v0-button";
+
+export { InstallationCommands, DependenciesInstallCommands, OpenInV0Button };
