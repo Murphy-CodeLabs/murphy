05a8bfff15f70de849cd8b7f3ee17d294714927a
lou1s <lou1svuong.dev@gmail.com>
2025-04-20 21:11:00 +0700

    Enhance documentation exports by adding PreviewComponent
    - Added the PreviewComponent to the exports in the index.tsx file for improved documentation functionality.
    - Updated the export structure to include the new component alongside existing ones.

--- components/docs/index.tsx
+++ components/docs/index.tsx
@@ -1,5 +1,11 @@
 import InstallationCommands from "./installation-commands";
 import DependenciesInstallCommands from "./dependencies-install-commands";
 import OpenInV0Button from "./open-in-v0-button";
+import { PreviewComponent } from "./preview-component";
 
-export { InstallationCommands, DependenciesInstallCommands, OpenInV0Button };
+export {
+  InstallationCommands,
+  DependenciesInstallCommands,
+  OpenInV0Button,
+  PreviewComponent,
+};
