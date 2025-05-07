1d511f0574988f4564014198ca5adebc89551e6b
lou1s <lou1svuong.dev@gmail.com>
2025-05-07 14:32:07 +0700

    style: update button and container styles in installation commands documentation

--- components/docs/installation-commands.tsx
+++ components/docs/installation-commands.tsx
@@ -65,7 +65,7 @@ export default function InstallationCommands({
   };
 
   return (
-    <div className="rounded-lg overflow-hidden">
+    <div className="rounded-lg border overflow-hidden">
       <div className="bg-fd-background text-white">
         <Tabs defaultValue="pnpm">
           <TabsList className="bg-fd-background rounded-none h-12 px-2 w-full justify-between">
@@ -111,7 +111,7 @@ export default function InstallationCommands({
             <Button
               variant="ghost"
               size="icon"
-              className="text-white hover:bg-zinc-800 rounded-md hover:text-white"
+              className="rounded-md"
               onClick={handleCopy}
             >
               {copied ? (
