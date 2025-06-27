6149d2d8ff61873544087acf5b833c019cd41461
lou1s <lou1svuong.dev@gmail.com>
2025-06-27 10:32:39 +0700

    feat: add TutorialVideoDialog component and update documentation

--- components/docs/index.tsx
+++ components/docs/index.tsx
@@ -2,10 +2,12 @@ import InstallationCommands from "./installation-commands";
 import DependenciesInstallCommands from "./dependencies-install-commands";
 import OpenInV0Button from "./open-in-v0-button";
 import { PreviewComponent } from "./preview-component";
+import { TutorialVideoDialog } from "./tutorial-video-dialog";
 
 export {
   InstallationCommands,
   DependenciesInstallCommands,
   OpenInV0Button,
   PreviewComponent,
+  TutorialVideoDialog,
 };
