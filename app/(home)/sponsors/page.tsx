692f2075198c850d4644a19bacdc70634e0c786e
lou1s <lou1svuong.dev@gmail.com>
2025-07-27 08:29:25 +0700

    refactor: update layout configuration, remove unused components, and enhance documentation links

--- app/(home)/sponsors/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Sponsors() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1>Sponsors</h1>
-      <p>Coming soon...</p>
-    </div>
-  );
-}
